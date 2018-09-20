d847678e488c44464b56d965e764bf84e92c24b5
SerVB <servbul@yandex.ru>
2018-09-20 12:44:04 +0300

    Убраны методы сохранения по умолчанию

--- sample_launcher_all_mag.py
+++ sample_launcher_all_mag.py
@@ -1,8 +1,6 @@
 # encoding=utf-8
 
-from common_json import writeJsonPerUniversity
-
-import ranepa_mag; ranepa_mag.main(saveMethods=(writeJsonPerUniversity,))
-import spbu_mag; spbu_mag.main(saveMethods=(writeJsonPerUniversity,))
-import rggu_mag; rggu_mag.main(saveMethods=(writeJsonPerUniversity,))
-import msu_mag; msu_mag.main(saveMethods=(writeJsonPerUniversity,))
+import ranepa_mag; ranepa_mag.main()
+import spbu_mag; spbu_mag.main()
+import rggu_mag; rggu_mag.main()
+import msu_mag; msu_mag.main()
